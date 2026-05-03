2f6cfe1933bb6092e8f0f9ff392b7a9f75c928c6
yashfeenfatima494 <yashfeen494@gmail.com>
2026-05-03 10:12:41 +0530

    portfolio chnges

---
+++ src/data/portfolioData.jsx
@@ -0,0 +1,205 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaCode,
+  FaHtml5,
+  FaCss3Alt,
+  FaJava,
+} from "react-icons/fa";
+import {
+  SiExpress,
+  SiMongodb,
+  SiFirebase,
+  SiTailwindcss,
+  SiJavascript,
+  SiCplusplus,
+  SiSpringboot,
+  SiPostman,
+  SiGooglecloud,
+  SiTypescript,
+  SiFigma,
+  SiNpm,
+  SiNextdotjs,
+} from "react-icons/si";
+
+export const personalInfo = {
+  name: "Asad Raiyan",
+  title: "Frontend Developer",
+  email: "adsadraiyan001@gmail.com",
+  linkedin: "https://www.linkedin.com/in/asad-raiyan",
+  github: "https://github.com/asadraiyan",
+  resumeLink: "",
+  bio: "Innovative Frontend Developer crafting seamless and high-performance web experiences. With 4+ years of experience in React.js, Next.js and modern JavaScript ecosystems, I specialize in building scalable, user-centric interfaces. I thrive in collaborative environments, translating complex ideas into intuitive digital solutions while maintaining a strong focus on performance, usability, and clean code.",
+  shortBio: "I build things for the web.",
+};
+
+export const education = [
+  {
+    institution: "Chandigarh University",
+    degree: "Bachelor of Engineering in Computer Science",
+    duration: "Aug 2023 – Present",
+    score: "CGPA: 8.04/10",
+  },
+  {
+    institution: "Namdev Public School",
+    degree: "Class 12th CBSE",
+    duration: "April 2022 - May 2023",
+    score: "96.5%",
+  },
+  {
+    institution: "Namdev Public School",
+    degree: "Class 10th CBSE",
+    duration: "April 2020 - May 2021",
+    score: "95.17%",
+  },
+];
+
+export const projects = [
+  {
+    title: "MovieFlix",
+    tech: ["React JS"],
+    description: [
+      <>
+        Design & Developed{" "}
+        <span className="text-text-primary font-medium">
+          a feature-rich movie application
+        </span>{" "}
+        using React.js, enabling users to search, browse, and discover movies seamlessly.
+      </>,
+      <>
+        Integrated the{" "}
+        <span className="text-text-primary font-medium">
+          TMDB API
+        </span>{" "}
+        to display detailed movie information including ratings, reviews, and recommendations, with advanced filtering and sorting options for a personalized experience.
+      </>,
+    ],
+    githubLink: "https://github.com/asadraiyan/Movieflix",
+    liveLink: "https://movieflix-beta-black.vercel.app/",
+    category: "Full Stack",
+    icon: <FaReact size={24} className="text-accent-1" />,
+    image: "/moviflix.png",
+  },
+  {
+    title: "Unique Collection",
+    tech: ["React.js"],
+    description: [
+      <>
+        Built a{" "}
+        <span className="text-text-primary font-medium">
+          scalable e-commerce web application
+        </span>{" "}
+        using React.js, enabling users to browse and purchase products across categories like clothing, jewellery, and electronics.
+      </>,
+      <>
+        Implemented core features including{" "}
+        <span className="text-text-primary font-medium">
+          add-to-cart functionality, secure checkout, and payment integration
+        </span>
+        , along with dynamic product listings, filtering, and a fully responsive UI for a seamless shopping experience.
+      </>,
+    ],
+    githubLink: "https://github.com/asadraiyan/E-Mart",
+    liveLink: "https://unique-collection.netlify.app/",
+    category: "Frontend",
+    icon: <FaCode size={24} className="text-accent-1" />,
+    image: "unique.png",
+  },
+    {
+    title: "Annihilators",
+    tech: ["React.js, Node.js, Express.js, MongoDB"],
+   description: [
+  <>
+    Developed{" "}
+    <span className="text-text-primary font-medium">
+      Annihilators, a full-stack MERN cricket application
+    </span>{" "}
+    enabling users to explore detailed player statistics and cricket-related data.
+  </>,
+  <>
+    Implemented features such as{" "}
+    <span className="text-text-primary font-medium">
+      team selection, user profiles, and dynamic data rendering
+    </span>{" "}
+    to provide a comprehensive and interactive cricket management experience.
+  </>,
+],
+    githubLink: "https://github.com/Annihilators-lucknow",
+    liveLink: "https://beta-annihilators.netlify.app/",
+    category: "Frontend",
+    icon: <FaCode size={24} className="text-accent-1" />,
+    image: "cricket.png",
+  },
+];
+
+export const skills = {
+  languages: [
+    { name: "JavaScript", icon: <SiJavascript /> },
+    { name: "TypeScript", icon: <SiTypescript /> },
+    { name: "HTML5", icon: <FaHtml5 /> },
+    { name: "CSS3", icon: <FaCss3Alt /> },
+  ],
+  frameworksAndLibraries: [
+    { name: "React JS", icon: <FaReact /> },
+    { name: "Next JS", icon: <SiNextdotjs /> },
+    { name: "Node JS", icon: <FaNodeJs /> },
+    { name: "Express JS", icon: <SiExpress /> },
+    { name: "React Native", icon: <FaReact /> },
+  ],
+  toolsAndPlatforms: [
+    { name: "Git & GitHub", icon: <FaGithub /> },
+    { name: "MongoDB", icon: <SiMongodb /> },
+    { name: "n8n", icon: <FaCode /> },
+    { name: "NPM", icon: <SiNpm /> },
+    { name: "Postman", icon: <SiPostman /> },
+    { name: "Figma", icon: <SiFigma /> },
+  ],
+  coreCompetencies: [
+    "Presentations",
+    "Oratory",
+    "Team Leadership",
+    "Event Hosting",
+    "Agile Methodologies",
+    "Problem Solving",
+  ],
+};
+
+export const experience = [
+  {
+    role: "Software Developer",
+    organization: "Quarks Technosoft (Noida)",
+    duration: "October 2024 – Present",
+    points: [
+      "Contributed to frontend development for Livemint (CRIF & Credilio) and Hindustan Times, building scalable React.js/Next.js UIs with reusable components",
+      " Developed modular JavaScript SDKs (Login & Voucher) for cross-platform integration across HT, Live Hindustan, and Livemint",
+      "Built a Vendor Management System dashboard from scratch using React.js, TypeScript, Redux Toolkit, and MUI",
+      "Implemented Admin & Vendor modules covering onboarding, SOW/PO, invoices, and resource management",
+      "Conducted code reviews and mentored juniors to maintain code quality and best practices",
+    ],
+  },
+  {
+    role: "Frontend Developer",
+    organization: "Swagg Technologies (Remote)",
+    duration: "March 2022 – October 2024",
+    points: [
+      "Developed and maintained an e-commerce dashboard using React.js, delivering real-time insights with responsive UI and effi cient state management",
+      "Collaborated with cross-functional teams to build features, optimize performance, and ensure scalability and reliability",
+      "Actively participated in design and code reviews to maintain high-quality, requirement-aligned code",
+      "Translated Figma designs into functional React.js components with pixel-perfect accuracy",
+      "Diagnosed and resolved bugs effi ciently, improving application stability and performance",
+    ],
+  },
+];
+
+export const socialLinks = {
+  linkedin: { url: personalInfo.linkedin, icon: <FaLinkedin size={24} /> },
+  github: { url: personalInfo.github, icon: <FaGithub size={24} /> },
+  email: {
+    url: `mailto:${personalInfo.email}`,
+    icon: <FaEnvelope size={24} />,
+  },
+};
